c80a2ed64e8422478dbc09a9b3b9344a31159fcd
luoye <842891024@qq.com>
2016-08-21 01:39:21 +0800

    win32 builder test

--- src/main/app.babel.js
+++ src/main/app.babel.js
@@ -1,4 +1,8 @@
 // babel entrance
-require('babel-core/register')();
+require('babel-core/register')({
+	'presets': ['es2015', 'stage-2'],
+	'ignore': ['*.asar'],
+	'plugins': ['transform-runtime']
+});
 
 require('./app.js');
