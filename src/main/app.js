e75c77b776a81586f06e6500bc8a39d02a8de8ab
luoye <842891024@qq.com>
2016-08-20 18:47:43 +0800

    del will-all-closed event

--- src/main/app.js
+++ src/main/app.js
@@ -4,10 +4,6 @@ import createWindow from './window/create.js';
 
 app.on('ready', createWindow);
 
-app.on('window-all-closed', () => {
-	// just for hold icon in dock
-});
-
 app.on('activate', () => {
 	createWindow();
 });
