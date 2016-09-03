c8c93aed20af6e6b914fb4d97d09626cb4d3bb1f
luoye <842891024@qq.com>
2016-09-04 00:03:17 +0800

    change update check way from github to qiniu

--- src/main/ipc/update.js
+++ src/main/ipc/update.js
@@ -15,51 +15,46 @@ ipcMain.on('app-init', (ev) => {
 		msg: '正在检查更新',
 		loading: true
 	});
-	fetch('https://api.github.com/repos/gomeplusFED/FET/releases', {
+	fetch('http://7xqahl.com1.z0.glb.clouddn.com/info.json', {
 		method: 'GET',
 		headers: {
-			'Authorization': `Basic ${new Buffer('luoye-fe:aae6fbcc760f98fba79442620f26bf4127281c75').toString('base64')}`,
 			'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
 		},
 		timeout: 3000
 	}).then((res) => {
 		return res.json();
 	}).then((res) => {
-		if (res[0].tag_name === `v${appInfo.version}`) {
+		if (res.version === `v${appInfo.version}`) {
 			ev.sender.send('app-initing', {
 				msg: '已是最新版',
 				loading: false
 			});
 		} else {
-			res[0].assets.forEach((assetItem) => {
-				if (/asar/.test(assetItem.name)) {
-					tempWin = new BrowserWindow({
-						width: 0,
-						height: 0
-					});
-					tempWin.webContents.session.on('will-download', (event, item, webContents) => {
-						item.setSavePath(path.join(app.getAppPath(), '../' + assetItem.name));
-						item.on('updated', (event, state) => {
-							if (state === 'progressing') {
-								ev.sender.send('app-initing', {
-									msg: `正在下载 ${formatFileSize(item.getReceivedBytes())}/${formatFileSize(item.getTotalBytes())}`,
-									loading: true
-								});
-							}
-						});
-						item.once('done', (event, state) => {
-							if (state === 'completed') {
-								ev.sender.send('app-initing', {
-									msg: '下载完成，重启生效',
-									loading: false
-								});
-								fs.createReadStream(path.join(app.getAppPath(), '../' + assetItem.name)).pipe(path.join(app.getAppPath(), '../app.asar'));
-							}
-						});
-					});
-					tempWin.webContents.downloadURL('http://7xqahl.com1.z0.glb.clouddn.com/' + assetItem.name);
-				}
+			tempWin = new BrowserWindow({
+				width: 0,
+				height: 0
 			});
+			tempWin.webContents.session.on('will-download', (event, item, webContents) => {
+				item.setSavePath(path.join(app.getAppPath(), '../' + res.version));
+				item.on('updated', (event, state) => {
+					if (state === 'progressing') {
+						ev.sender.send('app-initing', {
+							msg: `正在下载 ${formatFileSize(item.getReceivedBytes())}/${formatFileSize(item.getTotalBytes())}`,
+							loading: true
+						});
+					}
+				});
+				item.once('done', (event, state) => {
+					if (state === 'completed') {
+						ev.sender.send('app-initing', {
+							msg: '下载完成，重启生效',
+							loading: false
+						});
+						fs.createReadStream(path.join(app.getAppPath(), '../' + res.version)).pipe(path.join(app.getAppPath(), '../app.asar'));
+					}
+				});
+			});
+			tempWin.webContents.downloadURL('http://7xqahl.com1.z0.glb.clouddn.com/' + res.version);
 		}
 	}).catch((e) => {
 		ev.sender.send('app-initing', {
